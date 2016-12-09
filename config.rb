99de266999f7e97d144fb45ad4866be40de9330f
yaotian <yaotian@gmail.com>
2016-12-09 15:39:05 +0800

    config中修改 删除原来的normalize.css， 这个用bootstrap的scss来生成

--- config.rb
+++ config.rb
@@ -4,10 +4,10 @@ require 'compass/import-once/activate'
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
-sass_dir = "sass"
+sass_dir = "scss"
 images_dir = "img"
 javascripts_dir = "js"
-fonts_dir = "css/fonts"
+fonts_dir = "fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
